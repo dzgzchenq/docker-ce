dc2a4d3a55ed57c7054e08eab507b9cf258c327a
Quan Chen <chenq182@sina.com>
2018-06-01 00:22:15 +0800

    2018-06-01

--- centos/install.sh
+++ centos/install.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-yum install ./libtool-ltdl-2.4.2-22.el7_3.x86_64.rpm
-yum install ./libseccomp-2.3.1-3.el7.x86_64.rpm
-yum install ./container-selinux-2.33-1.git86f33cd.el7.noarch.rpm
-yum install ./docker-ce-17.12.0.ce-1.el7.centos.x86_64.rpm
-systemctl start docker.service
-systemctl enable docker.service
-docker version
+mount /dev/vdb1 /mnt
 
 yum -y install vim-enhanced
 cp ./vimrc ~/.vimrc
 echo "alias vi='vim'" >> ~/.bashrc
+
+yum -y install git
+git config --global user.email "chenq182@sina.com"
+git config --global user.name "Quan Chen"
+
+yum -y install /mnt/docker-ce/centos/libtool-ltdl-2.4.2-22.el7_3.x86_64.rpm
+yum -y install /mnt/docker-ce/centos/libseccomp-2.3.1-3.el7.x86_64.rpm
+yum -y install /mnt/docker-ce/centos/container-selinux-2.33-1.git86f33cd.el7.noarch.rpm
+yum -y install /mnt/docker-ce/centos/docker-ce-17.12.0.ce-1.el7.centos.x86_64.rpm
+systemctl start docker.service
+systemctl enable docker.service
+cp /mnt/docker-ce/docker-compose /usr/local/bin/
+docker version
+
+umount /mnt
--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-dpkg -i ./docker-ce_17.12.0~ce-0~ubuntu_amd64.deb
+mount /dev/vdb1 /mnt
+
+echo -e "set nu\nset noai nosi" > ~/.vimrc
+sed -i 's/alF/lF/' ~/.bashrc
+
+git config --global user.email "chenq182@sina.com"
+git config --global user.name "Quan Chen"
+
+dpkg -i /mnt/docker-ce/docker-ce_17.12.0~ce-0~ubuntu_amd64.deb
+cp /mnt/docker-ce/docker-compose /usr/local/bin/
 docker version
+
+umount /mnt
