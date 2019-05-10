a65817e45472013bd14147807ff5d54584eb88b7
Quan Chen <chenq182@sina.com>
2019-05-10 18:18:46 +0800

    2019-05-10 18:18:46

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -8,8 +8,14 @@ sed -i 's/alF/lF/' ~/.bashrc
 git config --global user.email "chenq182@sina.com"
 git config --global user.name "Quan Chen"
 
+wget -c https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/containerd.io_1.2.5-1_amd64.deb
+wget -c https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce-cli_18.09.6~3-0~ubuntu-bionic_amd64.deb
+wget -c https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64/docker-ce_18.09.6~3-0~ubuntu-bionic_amd64.deb
 dpkg -i *.deb
-cp docker-compose /usr/local/bin/
+systemctl start docker.service
+systemctl enable docker.service
 docker version
+wget -c https://github.com/docker/compose/releases/download/1.24.0/docker-compose-Linux-x86_64
+cp docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
 
 umount /mnt
