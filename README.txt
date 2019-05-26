63078f5658c1b4877d5f3ccb169bc50abcebc4b7
Quan Chen <chenq182@sina.com>
2019-05-26 17:03:36 +0800

    2019-05-26 17:03:36

--- README.txt
+++ README.txt
@@ -1,7 +1,6 @@
-./centos/container-selinux-2.33-1.git86f33cd.el7.noarch.rpm
-./centos/docker-ce-17.12.0.ce-1.el7.centos.x86_64.rpm
-./centos/libseccomp-2.3.1-3.el7.x86_64.rpm
-./centos/libtool-ltdl-2.4.2-22.el7_3.x86_64.rpm
+./centos/containerd.io-1.2.5-3.1.el7.x86_64.rpm
+./centos/docker-ce-18.09.6-3.el7.x86_64.rpm
+./centos/docker-ce-cli-18.09.6-3.el7.x86_64.rpm
 ./ubuntu/containerd.io_1.2.5-1_amd64.deb
 ./ubuntu/docker-ce_18.09.6~3-0~ubuntu-bionic_amd64.deb
 ./ubuntu/docker-ce-cli_18.09.6~3-0~ubuntu-bionic_amd64.deb
--- centos/install.sh
+++ centos/install.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
+passwd
 mount /dev/vdb1 /mnt
-
+umount /mnt
 yum -y install vim-enhanced
-cp /mnt/docker-ce/centos/vimrc ~/.vimrc
+vi .vimrc
 echo "alias vi='vim'" >> ~/.bashrc
-
 yum -y install git
 git config --global user.email "chenq182@sina.com"
 git config --global user.name "Quan Chen"
-
-yum -y install /mnt/docker-ce/centos/libtool-ltdl-2.4.2-22.el7_3.x86_64.rpm
-yum -y install /mnt/docker-ce/centos/libseccomp-2.3.1-3.el7.x86_64.rpm
-yum -y install /mnt/docker-ce/centos/container-selinux-2.33-1.git86f33cd.el7.noarch.rpm
-yum -y install /mnt/docker-ce/centos/docker-ce-17.12.0.ce-1.el7.centos.x86_64.rpm
+wget -c https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.5-3.1.el7.x86_64.rpm
+wget -c https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-cli-18.09.6-3.el7.x86_64.rpm
+wget -c https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-18.09.6-3.el7.x86_64.rpm
+yum -y install ./containerd.io-1.2.5-3.1.el7.x86_64.rpm
+yum -y install ./docker-ce-cli-18.09.6-3.el7.x86_64.rpm
+yum -y install ./docker-ce-18.09.6-3.el7.x86_64.rpm
+rm *.rpm
 systemctl start docker.service
 systemctl enable docker.service
-cp /mnt/docker-ce/docker-compose /usr/local/bin/
-docker version
-
-umount /mnt
+wget -c https://github.com/docker/compose/releases/download/1.24.0/docker-compose-Linux-x86_64
+mv docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+chmod 555 /usr/local/bin/docker-compose
